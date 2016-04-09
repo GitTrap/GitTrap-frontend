49fb6c002f7e7a8c50c6d3f63c4e36d0c60290d3
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-09 14:17:10 -0400

    basic lists for feed and issues done.

--- app/components/Dashboard/Dashboard.js
+++ app/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import DashNav from './DashNav';
 import DashCharts from './DashCharts';
+import DashFeed from './DashFeed';
 
 export default class Dashboard extends Component {
   render() {
@@ -13,6 +14,12 @@ export default class Dashboard extends Component {
         </div>
         <div className="col-sm-10 nopadding segment">
           <DashCharts />
+          <div className="col-sm-6 nopadding">
+            <DashFeed listType="Follower Stream"/>
+          </div>
+          <div className="col-sm-6 nopadding">
+            <DashFeed listType="Issues"/>
+          </div>
         </div>
       </div>
     );
