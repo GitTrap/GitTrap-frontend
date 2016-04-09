2fd4f84127a2dde90695cff451d93ce70c2ab313
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-09 16:24:36 -0400

    added challenges file

---
+++ app/components/Challenges/Challenges.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+
+export default class LeaderBoard extends Component {
+  render() {
+    return (
+      <div className="Challenges">
+
+      </div>
+    );
+  }
+}
