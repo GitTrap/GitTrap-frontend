a5a6f91eccfc5eba86eef6bf4b7608f4783073b0
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-09 18:26:00 -0400

    basic styles and layouts for challenges

--- app/components/Challenges/Challenges.js
+++ app/components/Challenges/Challenges.js
@@ -7,7 +7,6 @@ export default class Challenges extends Component {
   render() {
     return (
       <div className="Challenges">
-          <h1>Challenges</h1>
           <div className="goal">
             <h1>Goal</h1>
             <ol>
@@ -15,6 +14,37 @@ export default class Challenges extends Component {
               <li>something 2</li>
               <li>something 3</li>
             </ol>
+          </div>
+          <div className="users">
+            <div className="challengers">
+              <img  src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/629c1f14015191.5627be627c443.png"/>
+              <div className="info">
+                <p>jalvarado91</p>
+                <p>commits: 200</p>
+                <p>Streak: 7</p>
+                <p>contributions: 4</p>
+              </div>
+            </div>
+
+            <div className="challengers">
+              <img  src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/629c1f14015191.5627be627c443.png"/>
+              <div className="info">
+                <p>jalvarado91</p>
+                <p>commits: 200</p>
+                <p>Streak: 7</p>
+                <p>contributions: 4</p>
+              </div>
+            </div>
+
+            <div className="challengers">
+              <img  src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/629c1f14015191.5627be627c443.png"/>
+              <div className="info">
+                <p>jalvarado91</p>
+                <p>commits: 200</p>
+                <p>Streak: 7</p>
+                <p>contributions: 4</p>
+              </div>
+            </div>
           </div>
       </div>
     );
