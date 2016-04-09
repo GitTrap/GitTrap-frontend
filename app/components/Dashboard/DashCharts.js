60eff8a17530062eff0f63a61d2397e2fe0afbf9
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-09 12:30:55 -0400

    update to charts

--- app/components/Dashboard/DashCharts.js
+++ app/components/Dashboard/DashCharts.js
@@ -1,10 +1,11 @@
 import './styles/DashCharts.scss';
 import React from 'react';
-import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area,
-         RadialBarChart, RadialBar, Legend,
-         RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
-         ResponsiveContainer
-       } from 'recharts';
+import {
+  AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area,
+  RadialBarChart, RadialBar, Legend,
+  RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
+  ResponsiveContainer
+ } from 'recharts';
 
 function DashCharts(props){
   const data = [
@@ -16,28 +17,28 @@ function DashCharts(props){
         {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
         {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
   ];
-  const data2 = [
-      {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
-      {name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed'},
-      {name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1'},
-      {name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d'},
-      {name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c'},
-      {name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57'},
-      {name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658'}
-    ];
-
-  const data3 = [
-      { subject: 'Math', A: 12, B: 11, fullMark: 150 },
-      { subject: 'Chinese', A: 9, B: 13, fullMark: 150 },
-      { subject: 'English', A: 8, B: 13, fullMark: 150 },
-      { subject: 'Geography', A: 9, B: 10, fullMark: 150 },
-      { subject: 'Physics', A: 8, B: 9, fullMark: 150 },
-      { subject: 'History', A: 6, B: 8, fullMark: 150 },
+  const data2= [
+        {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
+        {name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed'},
+        {name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1'},
+        {name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d'},
+        {name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c'},
+        {name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57'},
+        {name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658'}
+      ];
+   const data3 = [
+      { subject: 'Math', A: 120, B: 110, fullMark: 150 },
+      { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
+      { subject: 'English', A: 86, B: 130, fullMark: 150 },
+      { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
+      { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
+      { subject: 'History', A: 65, B: 85, fullMark: 150 },
   ];
 
   const style = {
-  	top: 0,
-  	left: 350,
+  	top: "50%",
+  	right: 0,
+    transform: "translate(0, -50%)",
   	lineHeight: '24px'
   };
   return (
@@ -56,14 +57,14 @@ function DashCharts(props){
       </ResponsiveContainer>
 
       <ResponsiveContainer width='100%' height='100%'>
-        <RadialBarChart className="radial" cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={10} data={data2}>
+        <RadialBarChart className="radial" cx="30%" cy="50%" innerRadius="10%" outerRadius="60%" data={data2}>
           <RadialBar minAngle={15} label background clockWise={true} dataKey='uv'/>
           <Legend iconSize={10} width={90} height={140} layout='vertical' verticalAlign='middle' wrapperStyle={style}/>
         </RadialBarChart>
       </ResponsiveContainer>
 
       <ResponsiveContainer width='100%' height='100%'>
-        <RadarChart className="radar" cx={300} cy={250} outerRadius={90} data={data3}>
+        <RadarChart className="radar" cx="50%" cy="50%" outerRadius="80%" data={data3}>
             <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
             <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}/>
             <PolarGrid />
