ede143facf7df84d0be62e6092cb4555fb3e9622
Juan Alvarado <jalvarado91@gmail.com>
2016-04-10 01:39:48 -0400

    folling layout

--- app/components/Users/Users.js
+++ app/components/Users/Users.js
@@ -1,11 +1,66 @@
+import './styles/Users.scss';
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 export default class Users extends Component {
   render() {
+    const following = [
+      {
+        id: 69156,
+        login: "chriscoyier",
+        gravatar_id: "",
+        url: "https://api.github.com/users/chriscoyier",
+        avatar_url: "https://avatars.githubusercontent.com/u/69156?"
+      },
+      {
+        id: 69156,
+        login: "chriscoyier",
+        gravatar_id: "",
+        url: "https://api.github.com/users/chriscoyier",
+        avatar_url: "https://avatars.githubusercontent.com/u/69156?"
+      },
+      {
+        id: 69156,
+        login: "chriscoyier",
+        gravatar_id: "",
+        url: "https://api.github.com/users/chriscoyier",
+        avatar_url: "https://avatars.githubusercontent.com/u/69156?"
+      },
+      {
+        id: 69156,
+        login: "chriscoyier",
+        gravatar_id: "",
+        url: "https://api.github.com/users/chriscoyier",
+        avatar_url: "https://avatars.githubusercontent.com/u/69156?"
+      },
+      {
+        id: 69156,
+        login: "chriscoyier",
+        gravatar_id: "",
+        url: "https://api.github.com/users/chriscoyier",
+        avatar_url: "https://avatars.githubusercontent.com/u/69156?"
+      }
+    ];
+
+    var users = following.map((person, key) => {
+      return (
+        <div key={key} className="user-item">
+          <img src={person.avatar_url}/>
+          <span className="user">{person.login}</span>
+          <div className="actions">
+            <a className="btn btn-default" href="#">Super Follow</a>
+            <a className="btn btn-warning" href="#">Unfollow</a>
+          </div>
+        </div>
+      );
+    });
+
     return (
-      <div className="">
-        <h1>Users</h1>
+      <div className="Users">
+        <h1>Following</h1>
+        <div className="narrow-container">
+          {users}
+        </div>
       </div>
     );
   }
